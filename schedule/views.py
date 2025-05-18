b1fee4dda31bf3e5e6eee2ec2043166379a1f8ef
greysuyen <carlambay687@gmail.com>
2025-05-18 10:20:25 +0800

    first commit

---
+++ schedule/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+import datetime
+
+def index(request):
+    now = datetime.datetime.now()
+    hour = now.hour
+    return render(request, "sched/index.html",{
+        "sched":(0 <= hour <= 6) or (17 <= hour <= 23)
+        
+    })
+
+print ("hi")
