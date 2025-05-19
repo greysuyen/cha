6a8da6d9b8319a260b62a2f842a35c116444c375
greysuyen <carlambay687@gmail.com>
2025-05-19 15:32:38 +0800

    Initial commit

--- schedule/views.py
+++ schedule/views.py
@@ -3,7 +3,6 @@ import datetime
 
 def index(request):
     now = datetime.datetime.now()
-    hour = now.hour
     return render(request, "sched/index.html",{
-        "sched":(0 <= hour <= 6) or (17 <= hour <= 23)
+        "sched": now.hour == 15
     })
